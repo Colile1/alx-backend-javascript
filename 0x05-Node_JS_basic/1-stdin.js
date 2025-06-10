fbed4ad9ddd6eb9654aefc5179d7d6d393f3df20
colile1 <colilesibanda@gmail.com>
2025-06-10 21:14:17 +0200

    0x05.js 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  process.exit();
 });
